7a1b43c0809be84dbb2426b6db462f943fed3e48
Natallia Lukyaniuk <nlukyaniuk@cpaglobal.com>
2019-11-04 15:45:50 +0300

    feat: change factory

--- src/js/components/factoryRequests.js
+++ src/js/components/factoryRequests.js
@@ -1,25 +1,26 @@
 class Request {
-	constructor(url, type) {
+	constructor(url, type, data) {
 	   this.url = url
 	   this.type = type
+	   this.data = data;
 	}
 
 	sendRequest() {
-		return fetch(this.url, { method: this.type});
+		return fetch(this.url, { method: this.type, body: this.data ? JSON.stringify(this.data): undefined });
 	}
 }
 
 export default class RequestsFactory {
-    createRequest(url, type) {
+    createRequest(url, type, data = null) {
         switch(type) {
 		    case 'GET':
-			    return new Request(url, type)
+			    return new Request(url, type, data)
 		    case 'PUT':
-			    return new Request(url, type)
+			    return new Request(url, type, data)
 			case 'POST':
-				return new Request(url, type)
+				return new Request(url, type, data)
 			case 'DELETE':
-				return new Request(url, type)
+				return new Request(url, type, data)
 	    }
     }
 }
